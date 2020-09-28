2db3e5398040a8f98c3636423c9a2ca4d67e2916
Ilaria Callegari <ilaria.callegari@gmail.com>
2020-09-28 19:16:33 +0100

    MatterJS boilerplate and configure grid, verticals and horizontals

---
+++ index.js
@@ -0,0 +1,47 @@
+//acquiring properties from Matter
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const cells = 3;
+const width = 600;
+const height = 600;
+
+//Boilerplate code
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: false,
+    width,
+    height,
+  },
+});
+
+//draw the updates of our world onto the screen
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+//walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+];
+
+World.add(world, walls);
+
+//Maze Generation
+//Grid
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
